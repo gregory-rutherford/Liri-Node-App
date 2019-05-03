d021dac38f20e6321478156a48ace98242d25b49
Gregory Rutherford <gregorford@gmail.com>
2019-05-03 14:12:50 -0500

    completed spotify but want to switch to case statements to help with other commands

--- liri.js
+++ liri.js
@@ -4,20 +4,22 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 var spotCommand = "spotify-this-song";
-var song = process.argv[3];
+let song = process.argv[3];
 
 if (spotCommand === process.argv[2]){
     // console.log(process.argv[3]);
     spotify.search({ type: 'track', query: song }, function(err, data) {
-        if (err) {
+        if (err) 
           return console.log('Error occurred: ' + err);
-        }
        for (var key in data) {
         //    console.log(data[key]);
            var dataObj = data[key];
            var name = dataObj.items[0].name;
+           var preview = dataObj.items[0].preview_url;
            var artist = dataObj.items[0].artists[0].name;
-           console.log(name, artist);
-        }
+           var album = dataObj.items[0].album.name;
+           console.log("'" + name + "'" + " by: ", artist, "\n", preview, "\n", album);
+        } 
       });
 }
+
