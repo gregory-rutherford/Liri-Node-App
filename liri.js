f9d54005dc7436c4355f3cc53c870a3c73d553ad
Gregory Rutherford <gregorford@gmail.com>
2019-05-02 20:16:35 -0500

    got some of the spotify info

--- liri.js
+++ liri.js
@@ -2,10 +2,22 @@ require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-console.log(spotify);
 
 var spotCommand = "spotify-this-song";
+var song = process.argv[3];
 
 if (spotCommand === process.argv[2]){
-    console.log()
+    // console.log(process.argv[3]);
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       for (var key in data) {
+        //    console.log(data[key]);
+           var dataObj = data[key];
+           var name = dataObj.items[0].name;
+           var artist = dataObj.items[0].artists[0].name;
+           console.log(name, artist);
+        }
+      });
 }
