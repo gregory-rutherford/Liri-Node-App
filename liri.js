10e9aa6caa884d46cf2ef9b69111cb76ec38a680
Gregory Rutherford <gregorford@gmail.com>
2019-05-07 18:30:00 -0500

    got movie search working

--- liri.js
+++ liri.js
@@ -5,9 +5,7 @@ const Spotify = require('node-spotify-api');
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify);
 const command = process.argv[2];
-var spotCommand = "spotify-this-song";
-var conCommand = "concert-this";
-var movieCommand = "movie-this";
+
 
 const userInput = process.argv;
 var inputArr = [];
@@ -51,49 +49,20 @@ break;
   });
   break;
   case "movie-this":
-  axios.get(`http://www.omdbapi.com/?apikey=trilogy&s=${input}`)
+  axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${input}`)
       .then(function(response){
         var data = response.data;
-        console.log(data)
+        let title = data.Title;
+        let year = data.Year;
+        let rating = data.Ratings[0].Value;
+        let rating2 = data.Ratings[1].Value;
+        let country = data.Production;
+        let lang = data.Language;
+        let plot = data.Plot;
+        let act = data.Actors;
+        console.log(title, year, rating, rating2, country, lang, plot, act);
       })
 break;
 default: 
 break;
 }
-
-// if (spotCommand === process.argv[2]){
-//     spotify.search({ type: 'track', query: input }, function(err, data) {
-//         if (err) 
-//           return console.log('Error occurred: ' + err);
-//        for (var key in data) {
-//         //    console.log(data[key]);
-//            var dataObj = data[key];
-//            var name = dataObj.items[0].name;
-//            var preview = dataObj.items[0].preview_url;
-//            var artist = dataObj.items[0].artists[0].name;
-//            var album = dataObj.items[0].album.name;
-//            console.log("'" + name + "'" + " by: ", artist, "\n", preview, "\n", album);
-//         } 
-//       });
-// } else if (conCommand === process.argv[2]){
-//   axios.get(`https://rest.bandsintown.com/artists/${process.argv[3]}/events?app_id=codingbootcamp`)
-//   .then(function (response) {
-//     console.log(response.data);
-//     var data = response.data;
-//     var venue = data[0].venue.name;
-//     var location = data[0].venue.city;
-//     var date = moment(data[0].datetime).format("MM/DD/YYYY");
-//     console.log(venue, location, date);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-// }
-
-// else if (movieCommand === process.argv[2]) {
-//     axios.get(`http://www.omdbapi.com/?apikey=trilogy&s=${input}`)
-//     .then(function(response){
-//       var data = response.data;
-//       console.log(data)
-//     })
-// }
