2d3f9e69d3ea8fedc0877542413c7b9f61d1c243
Gregory Rutherford <gregorford@gmail.com>
2019-05-08 14:24:45 -0500

    started using inqurier

--- liri.js
+++ liri.js
@@ -1,31 +1,40 @@
 require("dotenv").config();
+var inquirer = require("inquirer");
 const moment = require('moment');
 const axios = require("axios");
 const Spotify = require('node-spotify-api');
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify);
-const command = process.argv[2];
+// const command = process.argv[2];
+// const userInput = process.argv;
+// var inputArr = [];
+// for (var i = 3; i < userInput.length; i++){
+//   inputArr.push(userInput[i]);
+// }
+// var secondIt = inputArr.toLocaleString();
+// var input = secondIt.split(",");
 
-
-const userInput = process.argv;
-var inputArr = [];
-
-for (var i = 3; i < userInput.length; i++){
-  inputArr.push(userInput[i]);
-}
-var secondIt = inputArr.toLocaleString();
-var input = secondIt.split(",");
-
-
-
-switch (command) { 
+inquirer.prompt([
+  {
+    type: "list",
+    name: "choice",
+    message: "Please choose from the following options",
+    choices: ["spotify-this-song", "concert-this", "movie-this"]
+  },
+  {
+    type: "input",
+    name: "userInput",
+    message: "please declare your search",
+  }
+])
+.then(function(user){
+console.log(user.userInput);
+switch (user.choice) { 
   case "spotify-this-song":
-  spotify.search({ type: 'track', query: input }, function(err, data) {
-  // if (err) 
-  //         return console.log('Error occurred: ' + err);
+  spotify.search({ type: 'track', query: user.userInput }, function(err, data) {
+  if (err) 
+          return console.log('Sorry an error has occured: ' + err);
   for (var key in data) {
-
-    //    console.log(data[key]);
        var dataObj = data[key];
        var name = dataObj.items[0].name;
        var preview = dataObj.items[0].preview_url;
@@ -36,7 +45,7 @@ switch (command) {
   })
 break;
   case "concert-this":
-  axios.get(`https://rest.bandsintown.com/artists/${process.argv[3]}/events?app_id=codingbootcamp`)
+  axios.get(`https://rest.bandsintown.com/artists/${user.userInput}/events?app_id=codingbootcamp`)
   .then(function (response) {
     var data = response.data;
     var venue = data[0].venue.name;
@@ -45,11 +54,11 @@ break;
     console.log(venue, location, date);
   })
   .catch(function (error) {
-    console.log(error);
+    console.log('Sorry an error has occured' + error);
   });
   break;
   case "movie-this":
-  axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${input}`)
+  axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${user.userInput}`)
       .then(function(response){
         var data = response.data;
         let title = data.Title;
@@ -62,7 +71,11 @@ break;
         let act = data.Actors;
         console.log(title, year, rating, rating2, country, lang, plot, act);
       })
+      .catch(function (error) {
+        console.log('Sorry an error has occured' + error);
+      });
 break;
 default: 
 break;
 }
+});
